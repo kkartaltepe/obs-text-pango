f28aceaa44654a4e6e9febec396057d7b894f5ca
kilin <kilin@kilinbox.net>
2021-07-06 01:42:47 +0900

    Update text-pango.c
    Add alpha channel property

--- text-pango.c
+++ text-pango.c
@@ -307,9 +307,9 @@ static obs_properties_t *pango_source_get_properties(void *unused)
 		obs_module_text("Gradient"));
 	obs_property_set_modified_callback(prop,
 		pango_source_properties_gradient_changed);
-	obs_properties_add_color(props, "color1",
+	obs_properties_add_color_alpha(props, "color1",
 		obs_module_text("Gradient.Color"));
-	obs_properties_add_color(props, "color2",
+	obs_properties_add_color_alpha(props, "color2",
 		obs_module_text("Gradient.Color2"));
 
 	prop = obs_properties_add_list(props, "align",
